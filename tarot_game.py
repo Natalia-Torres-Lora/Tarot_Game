a5215010ab47a244af94ad5b73c03840adfcd46c
Natalia Torres <nataliatorres@Natas-Mac.local>
2025-01-23 22:47:35 -0500

    Added user inputs to start the card spread

--- tarot_game.py
+++ tarot_game.py
@@ -11,6 +11,7 @@ print('''
         This is your opportunity to dive deep into your past, present, and future, and confidently shape your destiny. Let\'s get started!
       ''')
 
+# Providing an option to the user to choose the type of reading they want
 option = input('''
         Step into the realm of insight and mystery. What truth do you seek to uncover?
 
@@ -21,6 +22,8 @@ option = input('''
             5. The Unfolding Journey (General Reading)
         
         Choose the number that calls to your soul." ''') 
+
+# Mapping the user's choice to the corresponding reading
 if option == '1':
     option = 'Love'
 elif option == '2':
@@ -30,8 +33,12 @@ elif option == '3':
 elif option == '4':
     option = 'Wealth'
 elif option == '5':
-    option = 'General Reading'
+    option = 'Unfolding Journey'
 else:
     print('Invalid input. Please choose a number from 1 to 5.')
     
-print(f'You have chosen to explore the realm of {option}. Let\'s begin your reading!')
+print(f'\nYou have chosen to explore the realm of {option}. Let\'s begin your reading!')
+
+# Initiating the thre card reading spread
+input('\nFocus your mind and form your question in the realm of the truth you chose. \nWhen ready, press Enter to reveal your cards:')
+
